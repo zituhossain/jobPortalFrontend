71faee641dcee0187982ddd4287a9ab94c3cda55
zituhossain5 <zituhossain@gmail.com>
2024-08-18 13:45:24 +0600

    filter jobs done

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from "react";
 import { Label } from "./ui/label";
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { useDispatch } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
 
 const filterData = [
   {
     filterType: "Location",
-    filterValue: ["Bangladesh", "Canada", "India", "USA"],
+    filterValue: ["Bangladesh", "Canada", "India", "USA", "UK", "Germany"],
   },
   {
     filterType: "Industry",
@@ -14,28 +17,52 @@ const filterData = [
       "Data Science",
       "Graphic Designer",
       "Full Stack Developer",
+      "Mobile App Developer",
+      "Software Engineer",
+      "Nextjs Developer",
+      "Laravel Developer",
+      "Flutter Developer",
+      "React Developer",
     ],
   },
-  {
-    filterType: "Salary",
-    filterValue: ["10k - 20k", "20k - 30k", "30k - 40k", "40k - 50k"],
-  },
+  // {
+  //   filterType: "Salary",
+  //   filterValue: [
+  //     "20k - 40k",
+  //     "40k - 60k",
+  //     "60k - 80k",
+  //     "80k - 100k",
+  //     "100k - 120k",
+  //   ],
+  // },
 ];
 
 const FilterCard = () => {
+  const dispatch = useDispatch();
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const changeHandler = (value) => {
+    setSelectedValue(value);
+  };
+
+  useEffect(() => {
+    dispatch(setSearchedQuery(selectedValue));
+  }, [selectedValue]);
+
   return (
     <div className="w-full bg-white p-3 rounded-md">
       <h1 className="font-bold text-lg">Filter Jobs</h1>
       <hr className="mt-3" />
-      <RadioGroup>
+      <RadioGroup value={selectedValue} onValueChange={changeHandler}>
         {filterData.map((data, index) => (
           <div key={index}>
             <h1 className="font-bold text-lg">{data?.filterType}</h1>
-            {data?.filterValue?.map((value, index) => {
+            {data?.filterValue?.map((value, idx) => {
+              const itemId = `id${index}-${idx}`;
               return (
                 <div key={index} className="flex items-center space-x-2 my-2">
-                  <RadioGroupItem value={value} />
-                  <Label>{value}</Label>
+                  <RadioGroupItem value={value} id={itemId} />
+                  <Label htmlFor={itemId}>{value}</Label>
                 </div>
               );
             })}
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -2,11 +2,28 @@ import { useSelector } from "react-redux";
 import FilterCard from "./FilterCard";
 import Job from "./Job";
 import Navbar from "./shared/Navbar";
+import { useEffect, useState } from "react";
 
 // const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Jobs = () => {
-  const { allJobs } = useSelector((store) => store.job);
+  const { allJobs, searchedQuery } = useSelector((store) => store.job);
+  const [filterJobs, setFilterJobs] = useState([]);
+
+  useEffect(() => {
+    const filteredJobs =
+      (allJobs?.length > 0 &&
+        allJobs.filter((job) => {
+          if (!searchedQuery) return true;
+          return (
+            job?.title?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+            job?.location?.toLowerCase().includes(searchedQuery.toLowerCase())
+          );
+        })) ||
+      [];
+
+    setFilterJobs(filteredJobs);
+  }, [searchedQuery]);
 
   return (
     <div>
@@ -16,12 +33,12 @@ const Jobs = () => {
           <div className="w-20%">
             <FilterCard />
           </div>
-          {allJobs.length <= 0 ? (
+          {filterJobs.length <= 0 ? (
             <span>Job not found</span>
           ) : (
             <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
               <div className="grid grid-cols-3 gap-4">
-                {allJobs.map((job) => (
+                {filterJobs.map((job) => (
                   <div key={job._id}>
                     <Job job={job} />
                   </div>
