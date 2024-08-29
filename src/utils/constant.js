de47461dc4c9bab197fd61cdc31f367e74dfce69
zituhossain5 <zituhossain@gmail.com>
2024-08-29 14:19:29 +0600

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,10 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT =
+  "https://jobportalbackend-8rzs.onrender.com/api/v1/user";
 
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT =
+  "https://jobportalbackend-8rzs.onrender.com/api/v1/job";
 
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://jobportalbackend-8rzs.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://jobportalbackend-8rzs.onrender.com/api/v1/company";
