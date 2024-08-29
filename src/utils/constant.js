22ba97d6ddc7124f90f475dba472620b0bcd8e79
zituhossain5 <zituhossain@gmail.com>
2024-08-29 12:12:16 +0600

    change api endpoint

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,10 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT =
+  "https://job-portal-backend-jet.vercel.app/api/v1/user";
 
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT =
+  "https://job-portal-backend-jet.vercel.app/api/v1/job";
 
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://job-portal-backend-jet.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-backend-jet.vercel.app/api/v1/company";
